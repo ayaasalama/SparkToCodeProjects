71dfa9bee05371f535999036995a3a9c6c9cf93d
Aya Salama <ayasl2905@gmail.com>
2026-06-30 23:47:10 +0400

    Practicing Diffierent Operations

--- CsharpSession1/Program.cs
+++ CsharpSession1/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            // Inputs and Outputs 
+
             Console.WriteLine("Hello, Spark to Code!");
 
             Console.WriteLine("Enter your name:");
@@ -16,6 +18,38 @@
             float salary = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Hello " + name + "!");
+
+            //////////////////////////////////////////////////////////////
+
+            // Operations 
+
+            Console.WriteLine("enter first number: ");
+            float firstNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter second number: ");
+            float secondNumber = float.Parse(Console.ReadLine());
+
+
+            float additionResult = firstNumber + secondNumber;
+            float subtractionResult = firstNumber - secondNumber;
+            float multiplicationResult = firstNumber * secondNumber;
+            float divisionResult = firstNumber / secondNumber;
+            float reminderResult = firstNumber % secondNumber;
+            bool comparisonResult = firstNumber == secondNumber;
+            bool testResult = !(10 > 5); // false
+
+            Console.WriteLine("Addition result: " + additionResult);
+            Console.WriteLine("Subtraction result: " + subtractionResult);
+            Console.WriteLine("Multiplication result: " + multiplicationResult);
+            Console.WriteLine("Division result: " + divisionResult);
+            Console.WriteLine("Reminder result: " + reminderResult);
+            Console.WriteLine("Comparison result: " + comparisonResult);
+            Console.WriteLine("Test result: " + testResult); 
+
+            ////////////////////////////////////////////////////////////////////////
+            
+            //
+
 
         }
     }

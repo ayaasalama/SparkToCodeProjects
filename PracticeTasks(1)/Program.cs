6ae009d93a8b50a851a1fd62aa7dbe22e40596d5
Aya Salama <ayasl2905@gmail.com>
2026-07-01 12:02:19 +0400

    Task 1 - Personal Info Card

---
+++ PracticeTasks(1)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticeTasks_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Personal Info Card
+
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height in meters");
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Are you a student? (True/False)");
+            bool isStudent = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height + ", Student: " + isStudent);
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+            //
+        }
+    }
+}

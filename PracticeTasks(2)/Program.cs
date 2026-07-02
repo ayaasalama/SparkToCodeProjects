18cc3f3b9607a3672c77568d13c8df6e98be3c90
Aya Salama <ayasl2905@gmail.com>
2026-07-02 11:50:19 +0400

    Task 1 - Countdown Timer

---
+++ PracticeTasks(2)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticeTasks_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Countdown Timer
+
+            Console.WriteLine("Countdown Timer");
+            Console.Write("Enter the Starting number: ");
+            int count = int.Parse(Console.ReadLine());
+            int i;
+
+            for (i = count; i >=1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
+
+            ////////////////////////////////////////////////////////////
+            
+            //
+        }
+    }
+}

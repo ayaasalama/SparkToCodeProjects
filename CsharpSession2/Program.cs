5b4ac22aa5ef99abc4cc74a96409309aae8b0b06
Aya Salama <ayasl2905@gmail.com>
2026-07-02 08:53:24 +0400

    For loop practice

---
+++ CsharpSession2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CsharpSession2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //For loop:
+
+            int counter = 0;
+
+            for (counter = 10; counter >= 5; counter--)
+            {
+                Console.WriteLine("certain message loop number:" + counter);
+
+            }
+
+            ///////////////////////////////////////////////////////////////////
+
+            
+
+        }
+    }
+}

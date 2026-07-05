adaff5fbec975329b2a0c8c53abfd73fb40e9223
Aya Salama <ayasl2905@gmail.com>
2026-07-06 00:21:37 +0400

    Implementing Task 1 - Absolute Differences

---
+++ PracticeTasks(3)/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PracticeTasks_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Absolute Difference
+
+            Console.Write("Enter the first number: ");
+            int num1 =int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 =int.Parse(Console.ReadLine());
+
+            int substract = num1 - num2;
+            Console.WriteLine("The substraction result is " + substract);
+            int absubstract = Math.Abs(substract);
+            Console.WriteLine("The positive differences: " + absubstract);
+
+        }
+    }
+}

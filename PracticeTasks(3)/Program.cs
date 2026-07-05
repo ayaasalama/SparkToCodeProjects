f1177a0bb9decb32b73801e3a17c0e9840980e69
Aya Salama <ayasl2905@gmail.com>
2026-07-06 03:16:56 +0400

    Implementing task 2 - power and root explorer

--- PracticeTasks(3)/Program.cs
+++ PracticeTasks(3)/Program.cs
@@ -6,16 +6,29 @@
         {
             // Task 1 - Absolute Difference
 
-            Console.Write("Enter the first number: ");
-            int num1 =int.Parse(Console.ReadLine());
+            //Console.Write("Enter the first number: ");
+            //int num1 =int.Parse(Console.ReadLine());
 
-            Console.Write("Enter the second number: ");
-            int num2 =int.Parse(Console.ReadLine());
+            //Console.Write("Enter the second number: ");
+            //int num2 =int.Parse(Console.ReadLine());
 
-            int substract = num1 - num2;
-            Console.WriteLine("The substraction result is " + substract);
-            int absubstract = Math.Abs(substract);
-            Console.WriteLine("The positive differences: " + absubstract);
+            //int substract = num1 - num2;
+            //Console.WriteLine("The substraction result is " + substract);
+            //int absubstract = Math.Abs(substract);
+            //Console.WriteLine("The positive differences: " + absubstract);
+
+            ///////////////////////////////////////////////////////////////
+
+            // Task 2 - Power & Root Explorer
+
+            Console.Write("Enter a number: ");
+            float num = float.Parse(Console.ReadLine());
+
+            double power = Math.Pow(num, 2);
+            double Sqrt = Math.Sqrt(num);
+
+            Console.WriteLine("The power of the number tp 2 is " + power + " and the square root is " + Sqrt);
+
 
         }
     }

7138130a2015641f543193e990c257d0e8715101
Aya Salama <ayasl2905@gmail.com>
2026-07-06 12:06:50 +0400

    Implementing Task 4 - Subscription End Date

--- PracticeTasks(3)/Program.cs
+++ PracticeTasks(3)/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace PracticeTasks_3_
+﻿using System.Globalization;
+
+namespace PracticeTasks_3_
 {
     internal class Program
     {
@@ -33,17 +35,33 @@
 
             // Task 3 - Name Formatter
 
-            Console.Write("Enter you full name: ");
-            string name = Console.ReadLine();
+            //Console.Write("Enter you full name: ");
+            //string name = Console.ReadLine();
 
-            string upperchar = name.ToUpper();
-            Console.WriteLine("Uppercase: " + upperchar);
+            //string upperchar = name.ToUpper();
+            //Console.WriteLine("Uppercase: " + upperchar);
 
-            string lowerchar = name.ToLower();
-            Console.WriteLine("Lowercase: " +  lowerchar);
+            //string lowerchar = name.ToLower();
+            //Console.WriteLine("Lowercase: " +  lowerchar);
 
-            int lettercount = name.Length;
-            Console.WriteLine("Number of characters: " + lettercount);
+            //int lettercount = name.Length;
+            //Console.WriteLine("Number of characters: " + lettercount);
+
+            //////////////////////////////////////////////////////////////////////////////////////////
+
+            // Task 4 - Subscription End Date
+
+            DateTime startDate = DateTime.Now;
+            Console.WriteLine("Start Date is: " + startDate);
+
+            Console.Write("Enter number of days of a free trial: ");
+            int numDays = int.Parse(Console.ReadLine());
+
+            DateTime endDate = startDate.AddDays(numDays);
+            Console.WriteLine("The End date is: " + endDate);
+
+            string subend = endDate.ToString("yyyy-MM-dd");
+            Console.WriteLine("The End date in yyyy-MM-dd format: " + subend); 
 
 
         }

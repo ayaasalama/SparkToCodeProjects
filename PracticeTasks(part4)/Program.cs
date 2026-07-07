a0d8a4fb18895728539fb7b48014fae4c6ef33a7
Aya Salama <ayasl2905@gmail.com>
2026-07-07 21:11:42 +0400

    Implemented Task 1 - Personalized Welcome Function

---
+++ PracticeTasks(part4)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace PracticeTasks_part4_
+{
+    internal class Program
+    {
+        // Task 1 - Personalized Welcome Function
+        public static void PrintWelcome(string name)
+        {
+            Console.WriteLine("Welcome " + name);
+        }
+
+        //////////////////////////////////////////////////////
+        
+        //
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            PrintWelcome(name);
+        }
+    }
+}

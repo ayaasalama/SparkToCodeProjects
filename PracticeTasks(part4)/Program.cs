241050d0a397cc982ff5ae0627950d005dba27ef
Aya Salama <ayasl2905@gmail.com>
2026-07-07 21:54:03 +0400

    Implemented Task 2 - Square Number Function

--- PracticeTasks(part4)/Program.cs
+++ PracticeTasks(part4)/Program.cs
@@ -5,20 +5,33 @@ namespace PracticeTasks_part4_
     internal class Program
     {
         // Task 1 - Personalized Welcome Function
-        public static void PrintWelcome(string name)
-        {
-            Console.WriteLine("Welcome " + name);
-        }
+        //public static void PrintWelcome(string name)
+        //{
+        //    Console.WriteLine("Welcome " + name);
+        //}
 
         //////////////////////////////////////////////////////
         
-        //
+        // Task 2 - Square Number Function
+        public static int Square(int x)
+        {
+            int sqrt = x * x;
+            Console.WriteLine("The square is " + sqrt);
+            return sqrt;
+            
+        }
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name");
-            string name = Console.ReadLine();
-            PrintWelcome(name);
+            //Console.WriteLine("Enter your name");
+            //string name = Console.ReadLine();
+            //PrintWelcome(name);
+
+            Console.WriteLine("Enter a number ");
+            int x = int.Parse(Console.ReadLine());
+            Square(x);
+
+
         }
     }
 }

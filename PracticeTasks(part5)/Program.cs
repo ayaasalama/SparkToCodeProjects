3685a885f79bbe4b4f4df684ce630be04b330242
Aya Salama <ayasl2905@gmail.com>
2026-07-09 03:34:55 +0400

    Implementing Task 6 - Filtered Shopping List

--- PracticeTasks(part5)/Program.cs
+++ PracticeTasks(part5)/Program.cs
@@ -74,31 +74,68 @@
 
             // Task 5 - Array Grade Range
 
-            int[] Grades = new int[5];
+            //int[] Grades = new int[5];
 
-            int a;
-            for (a=0; a<5; a++)
-            {
-                Console.WriteLine("Enter a grade:");
-                Grades[a] = int.Parse(Console.ReadLine());
-            }
+            //int a;
+            //for (a=0; a<5; a++)
+            //{
+            //    Console.WriteLine("Enter a grade:");
+            //    Grades[a] = int.Parse(Console.ReadLine());
+            //}
 
-            Array.Sort(Grades);
+            //Array.Sort(Grades);
 
-            int sum = 0;
-            for (int b = 0; b < 5; b++)
-            {
-                sum += Grades[b];
-            }
-            double average = sum / 5.0;
+            //int sum = 0;
+            //for (int b = 0; b < 5; b++)
+            //{
+            //    sum += Grades[b];
+            //}
+            //double average = sum / 5.0;
 
-            Console.WriteLine("Lowest Grade: " + Grades[0]);
-            Console.WriteLine("Highest Grade: " + Grades[4]);
-            Console.WriteLine("Average Grade: " + average);
+            //Console.WriteLine("Lowest Grade: " + Grades[0]);
+            //Console.WriteLine("Highest Grade: " + Grades[4]);
+            //Console.WriteLine("Average Grade: " + average);
 
             ////////////////////////////////////////////////////
 
-            
+            // Task 6 - Filtered Shopping List
+
+            List<string> ShoppingList = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter an item to add (or type 'done' to stop): ");
+                string item = Console.ReadLine();
+
+                if (item.ToLower() == "done")
+                {
+                    break;
+                }
+
+                ShoppingList.Add(item);
+            }
+
+            Console.WriteLine("Shopping List Before Removal");
+            foreach (string listItem in ShoppingList)
+            {
+                Console.WriteLine("- " + listItem);
+            }
+
+            Console.WriteLine("Enter the name of the item you want to remove: ");
+            string itemToRemove = Console.ReadLine();
+
+            bool wasRemoved = ShoppingList.Remove(itemToRemove);
+
+            if (!wasRemoved)
+            {
+                Console.WriteLine($"Note: '{itemToRemove}' was not found in the list");
+            }
+
+            Console.WriteLine("Final Shopping List After Removal");
+            foreach (string listItem in ShoppingList)
+            {
+                Console.WriteLine("- " + listItem);
+            }
 
 
 

54472dc6879d965bbc7f5736ffef21e1a2cf97ff
Aya Salama <ayasl2905@gmail.com>
2026-07-09 02:36:30 +0400

    Implemeted Task 1 - Fixed Grades Array

---
+++ PracticeTasks(part5)/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PracticeTasks_part5_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Fixed Grades Array
+
+            int[] studentGrades = new int[5];
+
+            int i;
+            for (i=0; i<5; i++)
+            {
+                Console.WriteLine("Enter the grade:");
+                studentGrades[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("The entered grades are:");
+            foreach (int studentGrade in studentGrades)
+            {
+                Console.WriteLine(studentGrade);
+            }
+
+            //////////////////////////////////////////////////////////
+            
+            //
+
+        }
+    }
+}

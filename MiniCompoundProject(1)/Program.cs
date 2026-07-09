ad4c6089d160e6decc4119831df336c2374fa3e5
Aya Salama <ayasl2905@gmail.com>
2026-07-09 18:22:54 +0400

    added While loop logic

--- MiniCompoundProject(1)/Program.cs
+++ MiniCompoundProject(1)/Program.cs
@@ -12,9 +12,23 @@ namespace MiniCompoundProject_1_
 
         static void Main(string[] args)
         {
-            
-        }
+            bool exitApp = false;
 
-        
-    }
+            while (exitApp == false)
+            {
+                Console.WriteLine("\n===== Welcome to Spark Bank =====");
+                Console.WriteLine("1. Add New Account");
+                Console.WriteLine("2. Deposit Money");
+                Console.WriteLine("3. Withdraw Money");
+                Console.WriteLine("4. Show Balance");
+                Console.WriteLine("5. Transfer Amount");
+                Console.WriteLine("6. Search Accounts by Customer Name");
+                Console.WriteLine("7. Close an Account");
+                Console.WriteLine("8. Exit");
+                Console.Write("Choose an option: ");
+
+            }
+
+
+        }
 }

4824f950975a80a9d223d905d504affd722faad1
Aya Salama <ayasl2905@gmail.com>
2026-07-09 18:08:51 +0400

    Declaring the lists (data Storage)

---
+++ MiniCompoundProject(1)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+namespace MiniCompoundProject_1_
+{
+    internal class Program
+    {
+        //Shared Data Storage - declared at Class level 
+
+        static List<string> customerNames = new List<string>();
+        static List<string> accountNumbers = new List<string>();
+        static List<double> balances = new List<double>();
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+}

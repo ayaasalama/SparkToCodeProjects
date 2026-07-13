617bb963e037b9b029cd175554f16fe1ba29965f
Aya Salama <ayasl2905@gmail.com>
2026-07-13 12:15:09 +0400

    Adding the first class (BankAccount)

---
+++ OOP_Part(1)/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace OOP_Part_1_
+{
+    // First Class BankAccount
+    public class BankAccount
+    {
+        // Properties
+        public int AccountNumber { get; set; }
+        public string HolderName { get; set; }
+        public double Balance { get; set; }
+
+        // Methods
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+
+            SendEmail();
+        }
+        public void Withdraw(double amount)
+        {
+            if (Balance > amount)
+            {
+                Balance -= amount;
+
+                SendEmail();
+            }
+            else
+            {
+                Console.WriteLine("Balance Insufficient!");
+            }
+        }
+        public double CheckBalance()
+        {
+            PrintInformation();
+            
+            return Balance;
+        }
+        private void PrintInformation()
+        {
+            Console.WriteLine("HolderName: " + HolderName);
+            Console.WriteLine("Balance: " + Balance);
+        }
+        private void SendEmail()
+        {
+            Console.WriteLine("Sending an Email...");
+        }
+
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}

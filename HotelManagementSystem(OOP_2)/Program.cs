e5a0fe9e43e641e987254c891b230ae4b84090bb
Aya Salama <ayasl2905@gmail.com>
2026-07-17 10:51:46 +0400

    Implemented Class Guest

--- HotelManagementSystem(OOP_2)/Program.cs
+++ HotelManagementSystem(OOP_2)/Program.cs
@@ -40,6 +40,42 @@
 
     }
 
+    public class Guest
+    {
+        
+        public string GuestId { get; }
+        public string GuestName { get; set; }
+        public string RoomNumber { get; set; }
+        public string CheckInDate { get; set; }
+        public int TotalNights { get; set; }
+
+        // Constructor
+        public Guest(string guestId, string guestName, string checkInDate, int totalNights)
+        {
+            GuestId = guestId;
+            GuestName = guestName;
+            RoomNumber = "Not Assigned";
+            CheckInDate = checkInDate;
+            TotalNights = totalNights;
+        }
+
+        // Method
+        public void DisplayGuest()
+        {
+            Console.WriteLine("Guest ID     : " + GuestId);
+            Console.WriteLine("Guest Name   : " + GuestName);
+            Console.WriteLine("Room Number  : " + RoomNumber);
+            Console.WriteLine("Check-In Date: " + CheckInDate);
+            Console.WriteLine("Total Nights : " + TotalNights);
+        }
+
+        public double CalculateTotalCost(double pricePerNight)
+        {
+            return pricePerNight * TotalNights;
+        }
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)

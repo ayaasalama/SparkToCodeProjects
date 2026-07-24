abda9a34705da6750af2cc56346a97ed24fe9e86
Aya Salama <ayasl2905@gmail.com>
2026-07-24 14:11:58 +0400

    Created the employee table with Super_ssn as foriegn key

--- DatabaseTasks/company_database.sql
+++ DatabaseTasks/company_database.sql
@@ -1,1 +1,18 @@
 CREATE DATABASE CompanyDatabase;
+
+CREATE TABLE EMPLOYEE
+(
+    Ssn CHAR(9) PRIMARY KEY,
+    Fname VARCHAR(50),
+    Minit CHAR(1),
+    Lname VARCHAR(50),
+    Bdate DATE,
+    Address VARCHAR(255),
+    Sex CHAR(1),
+    Salary DECIMAL(10, 2),
+    Super_ssn CHAR(9),
+    Dno INT,
+
+    FOREIGN KEY (Super_ssn)
+        REFERENCES EMPLOYEE(Ssn)
+);

96641c22415566fea254f3d064f01405c422f3b8
Aya Salama <ayasl2905@gmail.com>
2026-07-25 23:57:26 +0400

    Added the full employee table

---
+++ EFCore(Part1)/models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_Part1_.models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public int EmployeeSsn { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public char EmployeeMinit {  get; set; }
+        public string EmployeeLastName { get; set; }
+        public DateTime Bdate { get; set; }
+        public string EmployeeAddress { get; set; }
+        public double EmployeeSalary { get; set; }
+    }
+}

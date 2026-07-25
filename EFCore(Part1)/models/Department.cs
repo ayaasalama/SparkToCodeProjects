a31037e99bfc72d34a52feb0863c57fd2d4d90c9
Aya Salama <ayasl2905@gmail.com>
2026-07-26 00:27:54 +0400

    added the department class

---
+++ EFCore(Part1)/models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_Part1_.models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public int DepartmentNumber { get; set; }
+        public string DepartmentName { get; set; }
+        public DateTime MsgStartDate  { get; set; }
+
+    }
+}

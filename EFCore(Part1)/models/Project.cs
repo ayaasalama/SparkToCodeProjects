ffa1cd712272e2921823b0602ba4c666d2a49344
Aya Salama <ayasl2905@gmail.com>
2026-07-26 00:46:51 +0400

    added the Project class

---
+++ EFCore(Part1)/models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_Part1_.models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public int ProjectNumber { get; set; }
+        public string ProjectLocation { get; set; }
+    }
+}

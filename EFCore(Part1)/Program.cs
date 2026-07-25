9f5f0cd492577ebced7fb99e485b4d2c7d8b3157
Aya Salama <ayasl2905@gmail.com>
2026-07-25 23:45:21 +0400

    Created the Project and setup NuGet

---
+++ EFCore(Part1)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore_Part1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

97b1a11f8d384bf1961c1e990c3b01e4b20d0230
Aya Salama <ayasl2905@gmail.com>
2026-07-26 17:48:18 +0400

    Adding employee1 data

--- EFCore(Part1)/Program.cs
+++ EFCore(Part1)/Program.cs
@@ -6,7 +6,19 @@ namespace EFCore_Part1_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProjectContext context = new ProjectContext();
+
+            // add data case 1 
+            Employee e1 = new Employee();
+            e1.EmployeeFirstName = "Aya";
+            e1.EmployeeMinit = 'A';
+            e1.EmployeeLastName = "Salama";
+            e1.Bdate = new DateTime(2004, 5, 29);
+            e1.EmployeeSalary = 280;
+            e1.EmployeeAddress = "Muscat";
+
+            context.employees.Add(e1);
+            context.SaveChanges();
         }
     }
 }

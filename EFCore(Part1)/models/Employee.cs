03e9075fee2e969fa4dd569fb851fc39fbec8ca4
Aya Salama <ayasl2905@gmail.com>
2026-07-28 00:17:45 +0400

    Deleted Migrations folder and the Db to work again with part 2 added the supervision self relationship to the employee

--- EFCore(Part1)/models/Employee.cs
+++ EFCore(Part1)/models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace EFCore_Part1_.models
@@ -7,6 +9,7 @@ namespace EFCore_Part1_.models
     public class Employee
     {
         public int EmployeeId { get; set; }
+        [Key]
         public int EmployeeSsn { get; set; }
         public string EmployeeFirstName { get; set; }
         public char EmployeeMinit {  get; set; }
@@ -14,5 +17,15 @@ namespace EFCore_Part1_.models
         public DateTime Bdate { get; set; }
         public string EmployeeAddress { get; set; }
         public double EmployeeSalary { get; set; }
+
+        // Supervision
+        [InverseProperty("supervisor")]
+        public List<Employee> supervisee { get; set; }
+
+
+        [ForeignKey("supervisor")]
+        public int SupervisorID { get; set; }
+        public Employee supervisor { get; set; }
+
     }
 }

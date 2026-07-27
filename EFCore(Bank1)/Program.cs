0532589506b98f38b8c07fbecfc8629fa15f802a
Aya Salama <ayasl2905@gmail.com>
2026-07-27 12:29:13 +0400

    created the BankAccount class

---
+++ EFCore(Bank1)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore_Bank1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ EFCore(Bank1)/models/BankAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_Bank1_.models
+{
+    public class BankAccount
+    {
+        public int AccountId { get; set; }
+        public string HolderName { get; set; }
+        public double Balance { get; set; }
+    }
+}

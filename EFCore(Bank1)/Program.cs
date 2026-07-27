055bf008ab0c1c409d98bce08f60625eb6a2df60
Aya Salama <ayasl2905@gmail.com>
2026-07-27 12:45:05 +0400

    adding Id in the class

--- EFCore(Bank1)/Program.cs
+++ EFCore(Bank1)/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace EFCore_Bank1_
+﻿using EFCore_Bank1_.models;
+
+namespace EFCore_Bank1_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
     }
 }
--- EFCore(Bank1)/models/BankAccount.cs
+++ EFCore(Bank1)/models/BankAccount.cs
@@ -6,6 +6,7 @@ namespace EFCore_Bank1_.models
 {
     public class BankAccount
     {
+        public int Id { get; set; }
         public int AccountId { get; set; }
         public string HolderName { get; set; }
         public double Balance { get; set; }

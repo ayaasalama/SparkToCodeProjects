87852e0ac731b8792fa13de6251c51d1c2d2c67b
Aya Salama <ayasl2905@gmail.com>
2026-07-28 15:06:13 +0400

    Added the outline of the menu, the while loop, the switch cases

--- ECommerceTask/Program.cs
+++ ECommerceTask/Program.cs
@@ -2,9 +2,66 @@
 {
     internal class Program
     {
+        static TaskContext context = new TaskContext();
+
+        static int loggedInUserId = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool exitApp = false;
+            while (!exitApp)
+            {
+                Console.WriteLine("\n===== E-Commerce Console App =====");
+                Console.WriteLine(" 1. Register New User");
+                Console.WriteLine(" 2. Login");
+                Console.WriteLine(" 3. Add New Category");
+                Console.WriteLine(" 4. Add New Product");
+                Console.WriteLine(" 5. View All Products");
+                Console.WriteLine(" 6. Place an Order");
+                Console.WriteLine(" 7. View My Orders");
+                Console.WriteLine(" 8. View Order Details");
+                Console.WriteLine(" 9. Add a Review for an Order");
+                Console.WriteLine("10. View All Reviews for a Product");
+                Console.WriteLine("11. Logout");
+                Console.WriteLine(" 0. Exit");
+                Console.Write("Enter your choice: ");
+                int choice;
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1: RegisterUser(); break;
+                    case 2: Login(); break;
+                    case 3: AddCategory(); break;
+                    case 4: AddProduct(); break;
+                    case 5: ViewAllProducts(); break;
+                    case 6: PlaceOrder(); break;
+                    case 7: ViewMyOrders(); break;
+                    case 8: ViewOrderDetails(); break;
+                    case 9: AddReview(); break;
+                    case 10: ViewReviewsForProduct(); break;
+                    case 11: Logout(); break;
+                    case 0:
+                        exitApp = true;
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        break;
+                }
+
+            }
         }
+
+
+
     }
 }

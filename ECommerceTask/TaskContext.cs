224dbc26af6cc4682150519b6364cde4e566e0e4
Aya Salama <ayasl2905@gmail.com>
2026-07-28 12:41:29 +0400

    created the TaskContext file

---
+++ ECommerceTask/TaskContext.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceTask.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceTask
+{
+    public class TaskContext: DbContext
+    {
+        public DbSet<Category> category {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server = DESKTOP-NB7NDOC\\SQLEXPRESS; Database= ECommerceDB; Trusted_Connection=True; " +
+                "TrustServerCertificate=True;"
+            );
+        }
+    }
+}

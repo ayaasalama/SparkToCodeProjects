01ce7f1e4de656e106bdb029d78e441bcf596345
Aya Salama <ayasl2905@gmail.com>
2026-07-28 12:45:06 +0400

    Created Product Class and added the Relation belongs to the CategoryId as FK

---
+++ ECommerceTask/models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ECommerceTask.models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName {  get; set; }
+        public double ProductPrice { get; set; }
+
+        // belongs_to 
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}

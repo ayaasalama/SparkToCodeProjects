6ea56994b8c43050752979b9df5913b196a6279f
Aya Salama <ayasl2905@gmail.com>
2026-07-29 23:53:45 +0400

    Created the Product and category classes and the ProjectContext

---
+++ WebAPIProject/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIProject.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public List<Product> products { get; set; }
+    }
+}
---
+++ WebAPIProject/ProjectContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIProject.Models;
+
+namespace WebAPIProject
+{
+    public class ProjectContext: DbContext
+    {
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+
+    }
+}

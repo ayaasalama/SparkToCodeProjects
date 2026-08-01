1f66a06d943b6ea0f2d4cb4434f0710717bd91d9
Aya Salama <ayasl2905@gmail.com>
2026-08-01 23:00:25 +0400

    added the ProjectContext, AddCategory and DeleteCategory in the CategoryController class

--- WebAPIProject/Controllers/CategoryController.cs
+++ WebAPIProject/Controllers/CategoryController.cs
@@ -1,6 +1,33 @@
-﻿namespace WebAPIProject.Controllers
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIProject.Models;
+
+namespace WebAPIProject.Controllers
 {
     public class CategoryController
     {
+        private ProjectContext context;
+        public CategoryController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        public void AddCategory(Category c)
+        {
+            context.categories.Add(c);
+            context.SaveChanges();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            Category c = context.categories.FirstOrDefault(c => c.CategoryId == id);
+            if (c == null)
+            {
+            }
+            else
+            {
+                context.products.Remove(p);
+            }
+
+        }
     }
 }

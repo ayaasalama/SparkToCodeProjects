16a402f0ee4dc0c78a770df74a0d3a9714759f73
Aya Salama <ayasl2905@gmail.com>
2026-08-02 23:31:19 +0400

    added [JsonIgnore]

--- WebAPIProject/Models/Category.cs
+++ WebAPIProject/Models/Category.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace WebAPIProject.Models
 {
     public class Category
     {
         [Key]
+        [JsonIgnore]
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
--- WebAPIProject/Models/Product.cs
+++ WebAPIProject/Models/Product.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace WebAPIProject.Models
 {
     public class Product
     {
         [Key]
+        [JsonIgnore]
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
